a7d23e51652cc55b74bb3ded11dd1bc6856d232b
Pushkardeep <just.pushkardeep@gmail.com>
2024-12-25 20:30:56 +0530

    updating vercel configs

--- course-zilla/vite.config.js
+++ course-zilla/vite.config.js
@@ -1,11 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // server: {
-  //   host: '0.0.0.0', // Makes the app accessible on your local network
-  //   port: 3000, // You can specify a port if you like
-  // },
+  server: {
+    hmr: {
+      overlay: false,
+    },
+  },
 });
